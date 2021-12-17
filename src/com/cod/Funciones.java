b22a0186ddd2441f150af2184b93634504d5d8c3
Jose Alejandro Pereiro Gabian <jpereirogabian@danielcastelao.org>
2021-12-17 10:51:24 +0100

    MostrarPista

--- src/com/cod/Funciones.java
+++ src/com/cod/Funciones.java
@@ -32,6 +32,21 @@ public class Funciones {
 
     public static String mostrarPista(Tablero tablero) {
         //String.charAt
-        return null;
+        String aleatorio=new String(tablero.getCifraAleatoria());
+        String secuencia=new String(tablero.getCifraSecuencia());
+        tablero.setCifraPista("-----");
+        String pista=new String(tablero.getCifraPista());
+        for(int i=0; i<5; i++) {
+            if (aleatorio.charAt(i) == secuencia.charAt(i)) {
+                pista.replace(pista.charAt(i), 'O');
+            } else {
+                for (int j = 0; j < 5; j++) {
+                    pista.replace(pista.charAt(i), 'X');
+                    break;
+                }
+            }
+        }
+        System.out.println(tablero.getCifraAleatoria());
+        return tablero.getCifraAleatoria();
     }
 }
